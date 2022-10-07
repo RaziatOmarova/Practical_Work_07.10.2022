ed8da1b1ccda55d144ea073af2c7336bc5ddf7c0
Raziat <raziatomarova0@gmail.com>
2022-10-07 23:46:37 +0300

    first commit

---
+++ Практическая работа 07.10.22г/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_работа_07._10._22г
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите начальный диапозон: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Введите конечный диапозон: ");
+            int numbertwo = int.Parse(Console.ReadLine());
+            int[] array = new int[10];
+            foreach (var item in List(array, number, numbertwo))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        public static int[] List(int[] array, int number, int numbertwo)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(number, numbertwo);
+            }
+            return array;
+        }
+    }
+}
